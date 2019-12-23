b359622982482513b47d07752e2f3bdc3914ef8b
Srivastava <monica.srivastava@accenture.com>
2019-12-23 11:23:45 +0530

    first commit

---
+++ src/test/java/com/example/demo/ProductRatingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductRatingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
